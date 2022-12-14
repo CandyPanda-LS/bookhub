d064683546aa456f5872596fed1cc24fd48cf9b5
Lasal Sandeepa Hettiarachchi <62637475+CandyPanda-LS@users.noreply.github.com>
2022-12-14 13:24:21 +0530

    Registration page UI

--- src/routes/RouteComponent.js
+++ src/routes/RouteComponent.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
 
 
 //common 
@@ -9,21 +9,28 @@ import CTAComponent from "../Components/CTAComponent";
 import FooterComponent from "../Components/FooterComponent";
 //Pages
 import HomePageComponent from "../Pages/HomePageComponent";
+import LoginPageComponent from "../Pages/LoginPageComponent";
+import RegistrationPageComponent from "../Pages/RegistrationPageComponent";
 
 
 function RouteComponent() {
   return (
+    <Router>
+        <div>
+                <NavbarComponent/>
+                <Routes>
+                    <Route path='/bookhub' element={<HomePageComponent/>} />
+                    <Route path='/' element={<HomePageComponent/>} />
+                    <Route path='/login' element={<LoginPageComponent/>} />
+                    <Route path='/register' element={<RegistrationPageComponent/>} />
+
+                </Routes>
+                
+                <CTAComponent/>
+                <FooterComponent/>
+            </div>
+    </Router>
     
-    <div>
-        <NavbarComponent/>
-        <Router>
-            <Route path="/" exact component={HomePageComponent} />
-        </Router>
-        {/* <HomePageComponent/> */}
-        
-        <CTAComponent/>
-        <FooterComponent/>
-    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,17 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
+        bannerBlue: '#040933',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
